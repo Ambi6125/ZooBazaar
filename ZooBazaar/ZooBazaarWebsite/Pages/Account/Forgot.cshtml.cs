57ca77bcf7d302ff6af0febcee5051faf16131ec
Vartic,Alex S <rss.extreme@gmail.com>
2022-11-18 09:00:07 +0100

    Fix

---
+++ ZooBazaar/ZooBazaarWebsite/Pages/Account/Forgot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZooBazaarWebsite.Pages.Account
+{
+    public class ForgotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
